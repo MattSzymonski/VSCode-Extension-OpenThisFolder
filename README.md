87043d40efe11a3b5ca9525b5de70e1cfb318b95
MattSzymonski <mateusz.szymonski@gmail.com>
2025-06-14 17:05:22 +0200

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Open This Folder
+
+This VS Code extension adds a context menu command to open the right-clicked folder in the **current window**, replacing the existing workspace.
+
+## Usage
+
+1. Right-click a folder in the Explorer file tree
+2. Click “Open This Folder”
+3. Folder will be opened in the current window
---
+++ src/extension.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	// Open This Folder command
+	const openThisFolderCommand = vscode.commands.registerCommand('open-this-folder.openThisFolder', async (uri: vscode.Uri) => {
+		if (uri && uri.fsPath) {
+			const folderUri = vscode.Uri.file(uri.fsPath);
+
+			const success = await vscode.commands.executeCommand('vscode.openThisFolder', folderUri, false); // false = open in current window
+
+			if (!success) {
+				vscode.window.showErrorMessage('Could not open folder in current window.');
+			}
+		} else {
+			vscode.window.showErrorMessage('No folder clicked.');
+		}
+	});
+
+	context.subscriptions.push(openThisFolderCommand);
+}
+
+export function deactivate() {}
